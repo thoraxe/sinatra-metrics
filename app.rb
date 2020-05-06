e2323b62c6c958ee5bbd033b41db797b2ca1ad53
Erik M Jacobs <erikmjacobs@gmail.com>
2020-05-06 14:10:19 -0400

    Adds prometheus support

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 # myapp.rb
 require 'sinatra/base'
+require 'prometheus/client'
+
 
 class MyApp < Sinatra::Application
+  # returns a default registry
+  prometheus = Prometheus::Client.registry
+  
+  # create a new counter metric
+  http_requests = Prometheus::Client::Counter.new(:http_requests, docstring: 'A counter of HTTP requests made')
+  # register the metric
+  prometheus.register(http_requests)
+
   get '/' do
+    http_requests.increment
     'Hello world!'
   end
 end
--- config.ru
+++ config.ru
@@ -1,2 +1,12 @@
+require 'rack'
+require 'prometheus/middleware/collector'
+require 'prometheus/middleware/exporter'
+
+use Rack::Deflater
+use Prometheus::Middleware::Collector
+use Prometheus::Middleware::Exporter
+
+run ->(_) { [200, {'Content-Type' => 'text/html'}, ['OK']] }
+
 require './app'
 run MyApp
